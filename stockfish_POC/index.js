545b9e3785032a077795f630cf699d3057c8d851
georgeUrsachi <george.ursachi@lenus.io>
2023-05-23 16:16:41 +0200

    Moved stockfish POC to own folder

--- stockfish_POC/index.js
+++ stockfish_POC/index.js
@@ -7,6 +7,6 @@ Stockfish().then((engine) => {
 
     engine.postMessage('uci')
     engine.postMessage('position fen 4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1.')
-    engine.postMessage('d')
+    engine.postMessaggie('d')
     engine.postMessage('go depth 5')
 })
