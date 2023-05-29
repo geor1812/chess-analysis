1800bfc30fe2370662bcd2efe135ea9f3b037dfa
georgeUrsachi <george.ursachi@lenus.io>
2023-05-29 18:49:41 +0200

    Implemented onDrag and onClick logic

--- chess-analysis-client/src/components/AnalysisBoardPage/Chessboard.tsx
+++ chess-analysis-client/src/components/AnalysisBoardPage/Chessboard.tsx
@@ -1,7 +1,93 @@
+import { useState, useEffect } from 'react'
 import Board from 'chessboardjsx'
+import { Chess } from 'chess.js'
 
 const Chessboard = () => {
-  return <Board width={850} position={'start'} />
+  const [game, setGame] = useState<any>()
+  const [fen, setFen] = useState('start')
+  const [history, setHistory] = useState([])
+  const [pieceSquare, setPieceSquare] = useState('')
+  const [squareStyles, setSquareStyles] = useState({})
+
+  useEffect(() => {
+    setGame(new Chess())
+  }, [])
+
+  const onDrop = ({
+    sourceSquare,
+    targetSquare,
+  }: {
+    sourceSquare: string
+    targetSquare: string
+  }) => {
+    try {
+      game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q',
+      })
+    } catch (error) {
+      return null
+    }
+
+    setFen(game.fen())
+    //setHistory(game.history({ verbose: true }))
+  }
+
+  const onSquareClick = (square: string) => {
+    setPieceSquare(square)
+    setSquareStyles(squareStyling({ pieceSquare, history }))
+
+    try {
+      game.move({
+        from: pieceSquare,
+        to: square,
+        promotion: 'q',
+      })
+    } catch (error) {
+      return null
+    }
+
+    setFen(game.fen())
+    //setHistory(game.history({ verbose: true }))
+    setPieceSquare('')
+  }
+
+  const onMouseOverSquare = (square: string) => {
+    const possibleMoves = game.moves({
+      square: square,
+      verbose: true,
+    })
+  }
+
+  return (
+    <Board
+      width={850}
+      position={fen}
+      onDrop={onDrop}
+      onSquareClick={onSquareClick}
+      onMouseOverSquare={onMouseOverSquare}
+    />
+  )
+}
+
+const squareStyling = ({ pieceSquare, history }) => {
+  const sourceSquare = history.length && history[history.length - 1].from
+  const targetSquare = history.length && history[history.length - 1].to
+
+  return {
+    [pieceSquare]: { backgroundColor: 'rgba(255, 255, 0, 0.4)' },
+    ...(history.length && {
+      [sourceSquare]: {
+        backgroundColor: 'rgba(255, 255, 0, 0.4)',
+      },
+    }),
+    ...(history.length && {
+      [targetSquare]: {
+        backgroundColor: 'rgba(255, 255, 0, 0.4)',
+      },
+    }),
+  }
 }
 
 export default Chessboard
